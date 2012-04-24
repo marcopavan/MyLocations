f5144e381d54374a4e5ef18277161a305fb0ce79
Marco Pavan <marcopavan83@gmail.com>
2012-04-24 21:38:47 +0200

    getLocation completed

--- MyLocations/MyLocations/CurrentLocationViewController.h
+++ MyLocations/MyLocations/CurrentLocationViewController.h
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.h
+//  CurrentLocationViewController.h
 //  MyLocations
 //
 //  Created by Marco Pavan on 24/04/12.
@@ -7,7 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface FirstViewController : UIViewController
+@interface CurrentLocationViewController : UIViewController <CLLocationManagerDelegate>
+
+@property (strong, nonatomic) IBOutlet UILabel *messageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *latitudeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *longitudeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *addressLabel;
+
+@property (strong, nonatomic) IBOutlet UIButton *tagButton;
+@property (strong, nonatomic) IBOutlet UIButton *getButton;
+
+- (IBAction)tagLocation:(id)sender;
+- (IBAction)getLocation:(id)sender;
 
 @end
--- MyLocations/MyLocations/CurrentLocationViewController.m
+++ MyLocations/MyLocations/CurrentLocationViewController.m
@@ -1,18 +1,35 @@
 //
-//  FirstViewController.m
+//  CurrentLocationViewController.m
 //  MyLocations
 //
 //  Created by Marco Pavan on 24/04/12.
 //  Copyright (c) 2012 mpsoftware. All rights reserved.
 //
 
-#import "FirstViewController.h"
+#import "CurrentLocationViewController.h"
 
-@interface FirstViewController ()
+@interface CurrentLocationViewController ()
 
 @end
 
-@implementation FirstViewController
+@implementation CurrentLocationViewController {
+    CLLocationManager *locationManager;
+    CLLocation *location;
+}
+
+@synthesize messageLabel;
+@synthesize latitudeLabel;
+@synthesize longitudeLabel;
+@synthesize addressLabel;
+@synthesize tagButton;
+@synthesize getButton;
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    if ((self = [super initWithCoder:aDecoder])) {
+        locationManager = [[CLLocationManager alloc] init];
+    }
+    return self;
+}
 
 - (void)viewDidLoad
 {
@@ -22,13 +39,55 @@
 
 - (void)viewDidUnload
 {
+    [self setMessageLabel:nil];
+    [self setLatitudeLabel:nil];
+    [self setLongitudeLabel:nil];
+    [self setAddressLabel:nil];
+    [self setTagButton:nil];
+    [self setGetButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)tagLocation:(id)sender {
+}
+
+- (IBAction)getLocation:(id)sender {
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+    [locationManager startUpdatingLocation];
+}
+
+-(void)updateLabels {
+    if (location != nil) {
+        self.messageLabel.text = @"GPS Coordinates";
+        self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f", location.coordinate.latitude];
+        self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f", location.coordinate.longitude];
+        self.tagButton.hidden = NO;
+    } else {
+        self.messageLabel.text = @"Press the Button to Start";
+        self.latitudeLabel.text = @"";
+        self.longitudeLabel.text = @"";
+        self.addressLabel.text = @"";
+        self.tagButton.hidden = YES;
+    }
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"didFailWithError %@", error);
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+    NSLog(@"didUpdateToLocation %@", newLocation);
+    location = newLocation;
+    [self updateLabels];
 }
 
 @end
